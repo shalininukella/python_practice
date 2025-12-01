0753491805e602a1b67070a93a982f193f472fb2
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2025-12-02 00:50:59 +0530

    explained the approach by adding comments

--- dsa/LL/cycleDetect.py
+++ dsa/LL/cycleDetect.py
@@ -4,12 +4,18 @@
 #         self.val = x
 #         self.next = None
 
+
+#If the list has no cycle, fast reaches None and we return False.
+#If the list has a cycle, the fast pointer must eventually meet the slow pointer, their meet is guaranteed.
+    
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fast = head
         slow = head
         while fast and fast.next and slow:
-
+#because fast.next.next would crash if fast.next is None, 
+    #AttributeError: 'NoneType' object has no attribute 'next'
+            
             fast = fast.next.next
             slow = slow.next
 
